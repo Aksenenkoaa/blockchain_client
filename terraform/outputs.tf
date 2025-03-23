b498cdf898041b595bff961d35d0f1ea6865d560
aleksey.aksenenko <aleksey.aksenenko@evomarine.ru>
2025-03-24 00:38:54 +0300

    outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,8 @@ output "ecs_cluster_name" {
 output "ecs_service_name" {
   value = aws_ecs_service.service.name
 }
+
+output "ecs_task_definition_arn" {
+  description = "The ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.task.arn
+}
