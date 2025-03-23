499e445c3fa0cace4f2a3d6a47b2c02828a2459a
aleksey.aksenenko <aleksey.aksenenko@evomarine.ru>
2025-03-23 13:49:37 +0300

    main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,86 @@
+provider "aws" {
+  region = "us-east-1" # Change to your preferred region
+}
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "blockchain-client-cluster"
+}
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = "blockchain-client-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "blockchain-client"
+      image     = "blockchain-client:latest" # Use your Docker image name
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.log_group.name
+          awslogs-region        = "us-east-1"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_service" "service" {
+  name            = "blockchain-client-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = aws_subnet.public.*.id
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = "blockchain-client"
+    container_port   = 8080
+  }
+
+  depends_on = [aws_lb_listener.listener]
+}
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name = "/ecs/blockchain-client"
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
